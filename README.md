87627da2c19bba1cf7495e0c9ad7a7ea115267a2
EstebanMarin <estebanmarin002@gmail.com>
2024-04-05 17:36:41 -0500

    Add list concatenation and comparison examples in chap1Notes.hs

---
+++ README.md
@@ -0,0 +1,2 @@
+# Learn your haskell notes
+
--- chap1Notes.hs
+++ chap1Notes.hs
@@ -12,6 +12,32 @@ other = map doubleMe (filter odd [10 .. 20])
 doubleBigger :: (Ord a, Num a) => a -> a
 doubleBigger x = if x > 100 then x else x * 2
 
+listConcat = [1, 2, 3, 4] ++ [4, 5, 6, 7]
+
+-- cons operator
+
+testString :: [Char]
+testString = 'A' : "small cat"
+
+testInt :: [Int]
+testInt = 5 : [8, 9]
+
+comparingList =
+  [3, 2, 3] < [3, 2, 5]
+
+--   GHCi, version 9.4.8: https://www.haskell.org/ghc/  :? for help
+-- ghci> [3, 2, 3] < [3, 2, 5]
+-- True
+-- ghci> [3, 2, 3] < [3, 2, 2]
+-- False
+-- ghci> [3, 2, 3] < [3, 2, 3]
+-- False
+-- ghci> [3, 2, 3] < [3, 1, 3]
+-- False
+-- ghci> [3, 2, 3] < [3, 3, 3]
+-- True
+-- ghci> [3, 2, 3] < [3, 3, 2]
+
 main = do
   putStrLn "Hello, everybody!"
   putStrLn
