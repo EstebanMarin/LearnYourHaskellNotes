85ae01dd7b0a193f5fa45a9b8a868e0f746e772e
EstebanMarin <estebanmarin002@gmail.com>
2024-04-06 19:27:19 -0500

    Add BMI calculation functions and describeList function in chap3.hs

--- chap3.hs
+++ chap3.hs
@@ -8,6 +8,35 @@ listCompre xs = [a + b | (a, b) <- xs]
 
 -- Gards
 
+bmiTell :: Double -> Double -> String
+bmiTell w h
+  | bmi < 2 = "You are fat"
+  | bmi > 3 = "You are fatter"
+  where
+    bmi = w / h ^ 2
+
+calcBmis :: (Fractional a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+  where
+    bmi w h = w / h ^ 2
+
+cylinder :: Double -> Double -> Double
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+   in sideArea + 2 * topArea
+
+-- let in list comprehensions
+
+calcBmis2 :: (Fractional a, Ord a) => [(a, a)] -> [a]
+calcBmis2 xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi > 0.25]
+
+-- case
+
+-- how can I make it exaustive?
+describeList :: [a] -> [Char]
+describeList xs = "The list is " ++ case xs of [] -> "empty"
+
 main :: IO ()
 main = do
-  putStrLn "hellp chapter 3"
+  putStrLn "hello chapter 3"
