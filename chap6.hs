d5c787db82b557012c3bb41d8ab162b28d546d19
EstebanMarin <estebanmarin002@gmail.com>
2024-04-07 18:25:59 -0500

    chap6

--- chap6.hs
+++ chap6.hs
@@ -1,3 +1,18 @@
+import Data.List
+
+nob2 :: (Eq a) => [a] -> Int
+nob2 = length . nub
+
+testnob2 = nob2 [1, 1, 1, 1, 1]
+
+-- counting words from a string
+
+countWord :: String -> [(String, Int)]
+countWord = map (\ws -> (head ws, length ws)) . group . sort . words
+
+testingCountWord :: [(String, Int)]
+testingCountWord = countWord "hey these are the words in the sentence"
+
 main :: IO ()
 main = do
   putStrLn "Hello chapter 6"
