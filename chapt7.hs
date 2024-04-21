a48f0949f0f4cf4af5acb88daadb09c5f9886515
EstebanMarin <estebanmarin002@gmail.com>
2024-04-21 11:51:36 -0500

    nice

--- chapt7.hs
+++ chapt7.hs
@@ -15,6 +15,44 @@ nudge (Circle (Point x1 y1) r) a b =
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b =
   Rectangle (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2 + b))
 
+data Person = Person
+  { firstName :: String,
+    lastName :: String,
+    age :: Int,
+    height :: Float,
+    phoneNumber :: String,
+    flavor :: String
+  }
+  deriving (Show)
+
+--
+
+-- Typeclass Eq from standard library
+-- class Eq a where
+--   (==) :: a -> a -> Bool
+--   (/=) :: a -> a -> Bool
+--  ....
+
+data TraficLight = Red | Yellow | Green
+
+instance Eq TraficLight where
+  (==) :: TraficLight -> TraficLight -> Bool
+  Red == Red = True
+  Green == Green = True
+  Yellow == Yellow = True
+  _ == _ = False
+  (/=) :: TraficLight -> TraficLight -> Bool
+  Red /= Red = False
+  Green /= Green = False
+  Yellow /= Yellow = False
+  _ /= _ = True
+
+instance Show TraficLight where
+  show :: TraficLight -> String
+  show Red = "This is Red"
+  show Green = "This is Green"
+  show Yellow = "This is Yellow"
+
 main :: IO ()
 main = do
   putStrLn "Hello chapter 7"
