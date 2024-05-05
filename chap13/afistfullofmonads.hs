bbdc5e083201a48c4fa14bfb0fd55f5d5e201277
EstebanMarin <estebanmarin002@gmail.com>
2024-05-05 10:27:50 -0500

    feat: Add MonadMine typeclass and implement instances for FunctorMine and ApplicativeMine

--- chap13/afistfullofmonads.hs
+++ chap13/afistfullofmonads.hs
@@ -1,3 +1,20 @@
+-- functor
+class FunctorMine f where
+  fmap1 :: (a -> b) -> f a -> f b
+
+class (FunctorMine f) => ApplicativeMine f where
+  pure1 :: a -> f a
+  (<*!>) :: f (a -> b) -> f a -> f b
+
+class (ApplicativeMine m) => MonadMine m where
+  return :: a -> m a
+
+  -- it takes a monadic value and a function that takes a normal value and returns a monadic value
+  -- and returns a monadic value
+  (>>=!) :: m a -> (a -> m b) -> m b
+  (>>!) :: m a -> m b -> m b
+  x >>! y = x >>=! const y
+
 main :: IO ()
 main = do
-  putStrLn "Hello, world!"
+  putStrLn "A fistful of monads"
